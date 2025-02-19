b96ae8a9236490c7e5bd032d74f7b8d4bc549dbb
server-craftsman <huyit2003@gmail.com>
2025-02-19 23:22:59 +0700

    [NguyenDanHuy_19.02.2025] Add note for validationMiddleware

--- src/core/middleware/validation.middleware.ts
+++ src/core/middleware/validation.middleware.ts
@@ -6,31 +6,31 @@ import { HttpStatus } from "../enums";
 import { HttpException } from "../exceptions";
 import { IError } from "../interfaces";
 
-const validationMiddleware = (type: any, skipMissingProperties = false): RequestHandler => {
+const validationMiddleware = (type: any, skipMissingProperties = false): RequestHandler => { // eslint-disable-line @typescript-eslint/no-explicit-any
     return (req: Request, res: Response, next: NextFunction) => {
-        validate(plainToInstance(type, req.body), { skipMissingProperties: skipMissingProperties }).then(
-            (errors: ValidationError[]) => {
+        validate(plainToInstance(type, req.body), { skipMissingProperties: skipMissingProperties }).then( // validate the request body against the class
+            (errors: ValidationError[]) => { // if there are errors
                 if (errors.length > 0) {
                     let errorResults: IError[] = [];
 
-                    const extractConstraints = (error: ValidationError) => {
-                        if (error.constraints) {
-                            Object.values(error.constraints || {}).forEach((message) => {
-                                errorResults.push({
+                    const extractConstraints = (error: ValidationError) => { // extract the constraints from the error
+                        if (error.constraints) { // if there are constraints
+                            Object.values(error.constraints || {}).forEach((message) => { // iterate through the constraints
+                                errorResults.push({ // add the error to the results
                                     message,
                                     field: error.property,
                                 });
                             });
                         }
-                        if (error.children && error.children.length > 0) {
-                            error.children.forEach((childError) => {
-                                extractConstraints(childError);
+                        if (error.children && error.children.length > 0) { // if there are children
+                            error.children.forEach((childError) => { // iterate through the children
+                                extractConstraints(childError); // extract the constraints from the child
                             });
                         }
                     };
 
-                    errors.forEach((error) => {
-                        extractConstraints(error);
+                    errors.forEach((error) => { // iterate through the errors
+                        extractConstraints(error); // extract the constraints from the error
                     });
 
                     next(new HttpException(HttpStatus.BAD_REQUEST, "", errorResults));
